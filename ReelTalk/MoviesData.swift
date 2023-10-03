eb0efc2995cb8ed6b58a9d1169b0d3f6c01bcf48
Syed Raza <syed.raza011@gmail.com>
2023-10-02 23:17:36 -0400

    reading mock Data & able to show image

---
+++ ReelTalk/MoviesData.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesData.swift
+//  ReelTalk
+//
+//  Created by Syed Raza on 10/2/23.
+//
+
+import Foundation
--- ReelTalk/Views/ColorChangingLinesView.swift
+++ ReelTalk/Views/ColorChangingLinesView.swift
@@ -4,61 +4,7 @@
 //
 //  Created by Syed Raza on 10/2/23.
 //
-//import SwiftUI
-//
-//struct ColorChangingLinesView: View {
-//    @State private var selectedPage = 1
-//
-//    var body: some View {
-//        VStack (spacing: -20){
-//            HStack(spacing: 15) {
-//                Rectangle()
-//                    .frame(width: 40, height: 5)
-//                    .foregroundColor(selectedPage == 1 ? .yellow : .black)
-//                    .onTapGesture {
-//                        selectPage(page: 1)
-//                    }
-//
-//                Rectangle()
-//                    .frame(width: 40, height: 5)
-//                    .foregroundColor(selectedPage == 2 ? .yellow : .black)
-//                    .onTapGesture {
-//                        selectPage(page: 2)
-//                    }
-//
-//                Rectangle()
-//                    .frame(width: 40, height: 5)
-//                    .foregroundColor(selectedPage == 3 ? .yellow : .black)
-//                    .onTapGesture {
-//                        selectPage(page: 3)
-//                    }
-//            }
-//        
-//
-//            TabView(selection: $selectedPage) {
-//                TopMoviesView()
-//                    .tag(1)
-//
-//                SelectedMoviesView()
-//                    .tag(2)
-//
-//                TopTvShowsView()
-//                    .tag(3)
-//            }
-//            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-//        }
-//    }
-//
-//    private func selectPage(page: Int) {
-//        selectedPage = page
-//    }
-//}
-//
-//    struct ColorChangingLinesView_Previews: PreviewProvider{
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
 import SwiftUI
 
 struct ColorChangingLinesView: View {
